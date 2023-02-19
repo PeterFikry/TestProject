5509a8bf95622a9fee1e6dfaebf9d9f52da713ff
PeterFikry <peterfikry11@gmail.com>
2023-02-18 20:28:46 -0500

    class27

---
+++ src/class25/Tester.java
@@ -0,0 +1,9 @@
+package class25;
+
+public class Tester {
+    public static void main(String[] args) {
+        OwnAble[] ownAbles={new Car(),new Dog()};       //we can use just one of the ownAble or movAble
+
+
+    }
+}
